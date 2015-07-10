3e8a063d64afef637ec3a15fd01a36fbe1dc59e4
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2015-07-09 23:39:42 -0400

    Update and rename fastenlt.sql to products.sql

--- products.sql
+++ products.sql
@@ -1,23 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.1.6
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 20, 2015 at 09:34 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `fastenlt`
+-- Database: `products`
 --
 CREATE DATABASE IF NOT EXISTS `fastenlt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `fastenlt`;
@@ -41,8 +29,8 @@ CREATE TABLE IF NOT EXISTS `product` (
 --
 
 INSERT INTO `product` (`productID`, `name`, `description`, `quantity`) VALUES
-(1, 'nuts', '250mm nuts', 10),
-(2, 'bolts', 'large bolts', 6);
+(1, 'lorem', 'lorem ipsum', 4),
+(2, 'dolor', 'dolor', 12);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
